8f0f6ea0ba48498fe7ac43afd0213fe30e62ff14
Ana Carolina <anacarolinaneta@hotmail.com>
2021-03-09 22:12:17 -0300

    hospedando bdd no heroku

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php  
-	$endereco = "DB_ICATS";
+	$endereco = "us-cdbr-east-03.cleardb.com";
 	$usuario = "b339a471c1c33a";
 	$senha = "7602bfd3";
-	$banco = "us-cdbr-east-03.cleardb.com";
+	$banco = "DB_ICATS";
 	$MYSQLi = new mysqli($endereco,$usuario,$senha,$banco,3306);
 	//aninha se eu esquecer alguma vez, lembre de mudar a porta para 3306 e colocar a senha como "" (no seu n tem senha) MAS DESSA VEZ EU LEMBREIII :)
 	if(mysqli_connect_errno()){
