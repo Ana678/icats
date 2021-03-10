39e5da7541b7ed3154b72c66db4395d3709e7c39
Ana Carolina <anacarolinaneta@hotmail.com>
2021-03-09 22:29:53 -0300

    hospedando bdd no heroku

--- design_cabecalho_user.php
+++ design_cabecalho_user.php
@@ -7,22 +7,22 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>ICATS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="/icats/assets/images/icon/img7.jpg">
-    <link rel="stylesheet" href="/icats/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/icats/assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/icats/assets/css/themify-icons.css">
-    <link rel="stylesheet" href="/icats/assets/css/metisMenu.css">
-    <link rel="stylesheet" href="/icats/assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="/icats/assets/css/slicknav.min.css">
+    <link rel="shortcut icon" type="image/png" href="/assets/images/icon/img7.jpg">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/themify-icons.css">
+    <link rel="stylesheet" href="/assets/css/metisMenu.css">
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/assets/css/slicknav.min.css">
     <!-- amchart css -->
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
     <!-- others css -->
-    <link rel="stylesheet" href="/icats/assets/css/typography.css">
-    <link rel="stylesheet" href="/icats/assets/css/default-css.css">
-    <link rel="stylesheet" href="/icats/assets/css/styles.css">
-    <link rel="stylesheet" href="/icats/assets/css/responsive.css">
+    <link rel="stylesheet" href="/assets/css/typography.css">
+    <link rel="stylesheet" href="/assets/css/default-css.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/responsive.css">
     <!-- modernizr css -->
-    <script src="/icats/assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    <script src="/assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
 <body>
@@ -35,16 +35,16 @@
         <!-- sidebar menu area start -->
         <div class="sidebar-menu">
             <div class="sidebar-header" style="background-color: white;">
-                <a href="/icats/tela_principal.php"><img src="/icats/assets/images/icon/img7.jpg" alt="logo" style="width: 80%;"></a><br>
+                <a href="/icats/tela_principal.php"><img src="/assets/images/icon/img7.jpg" alt="logo" style="width: 80%;"></a><br>
             </div>
             <div class="main-menu">
                 <div class="menu-inner">
                     <nav>
                         <ul class="metismenu" id="menu">
-                            <li><a href="/icats/tela_principal.php"><i class="ti-home"></i> <span>Home</span></a></li>
-                            <li><a href="/icats/gatos/lista_gatos.php"><i class="ti-menu-alt"></i> <span>Lista de gatos</span></a></li>
-                            <li><a href="/icats/gatos/tela_cadastro_gato.php"><i class="ti-github"></i> <span>Cadastrar gato</span></a></li>
-                            <li><a href="/icats/tela_sobre_nos.php"><i class="ti-help-alt"></i> <span>Sobre nós</span></a></li>
+                            <li><a href="/tela_principal.php"><i class="ti-home"></i> <span>Home</span></a></li>
+                            <li><a href="/gatos/lista_gatos.php"><i class="ti-menu-alt"></i> <span>Lista de gatos</span></a></li>
+                            <li><a href="/gatos/tela_cadastro_gato.php"><i class="ti-github"></i> <span>Cadastrar gato</span></a></li>
+                            <li><a href="/tela_sobre_nos.php"><i class="ti-help-alt"></i> <span>Sobre nós</span></a></li>
                             
                         </ul>
                     </nav>
@@ -72,8 +72,8 @@
                             <li><div class="user-profile pull-right">
                                 <h4 class="user-name dropdown-toggle" data-toggle="dropdown"><?php $primeiroNome = explode(" ", $_SESSION['username']); echo current($primeiroNome); ?> <i class="fa fa-angle-down"></i></h4>
                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="/icats/user/tela_perfil_user.php">Perfil</a>
-                                    <a class="dropdown-item" href="/icats/login/logout.php">Log Out</a>
+                                    <a class="dropdown-item" href="/user/tela_perfil_user.php">Perfil</a>
+                                    <a class="dropdown-item" href="/login/logout.php">Log Out</a>
                                 </div>
                             </div></li>  
                         </ul>
