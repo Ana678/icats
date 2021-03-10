cafd8208087b369b2bb83c8a57f05c7763b34d11
Ana Carolina <anacarolinaneta@hotmail.com>
2021-03-09 22:00:46 -0300

    hospedando bdd no heroku

--- db_icats.sql
+++ db_icats.sql
@@ -1,7 +1,21 @@
 CREATE DATABASE IF NOT EXISTS DB_ICATS;
 USE DB_ICATS;
 
-CREATE TABLE `tb_sexos` (
+--
+-- Estrutura da tabela `tb_usuarios`
+--
+
+CREATE TABLE `TB_USUARIOS` (
+  `USU_CODIGO` int(11) NOT NULL auto_increment PRIMARY KEY,
+  `USU_EMAIL` varchar(45) NOT NULL,
+  `USU_NOME` varchar(80) NOT NULL,
+  `USU_SENHA` varchar(10) NOT NULL,
+  `USU_TELEFONE` varchar(45) NOT NULL,
+  `USU_FOTO` varchar(40)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+CREATE TABLE `TB_SEXOS` (
   `SEX_CODIGO` int(11) NOT NULL auto_increment PRIMARY KEY,
   `SEX_SEXO` varchar(9) NOT NULL
 
@@ -11,7 +25,7 @@ CREATE TABLE `tb_sexos` (
 -- Extraindo dados da tabela `tb_sexos `
 --
 
-INSERT INTO `tb_sexos` (`SEX_CODIGO`, `SEX_SEXO`) VALUES
+INSERT INTO `TB_SEXOS` (`SEX_CODIGO`, `SEX_SEXO`) VALUES
 (1, 'Feminino'),
 (2, 'Masculino');
 
@@ -20,7 +34,7 @@ INSERT INTO `tb_sexos` (`SEX_CODIGO`, `SEX_SEXO`) VALUES
 -- Estrutura da tabela `tb_gatos`
 --
 
-CREATE TABLE `tb_gatos` (
+CREATE TABLE `TB_GATOS` (
   `GAT_CODIGO` int(11) NOT NULL auto_increment PRIMARY KEY,
   `GAT_NOME` varchar(80) NOT NULL,
   `GAT_IDADE` varchar(80) NOT NULL,
@@ -32,7 +46,7 @@ CREATE TABLE `tb_gatos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
-CREATE TABLE `tb_humores` (
+CREATE TABLE `TB_HUMORES` (
   `HUM_CODIGO` int(11) NOT NULL auto_increment PRIMARY KEY,
   `HUM_HUMOR` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,7 +55,7 @@ CREATE TABLE `tb_humores` (
 -- Extraindo dados da tabela `tb_humores`
 --
 
-INSERT INTO `tb_humores` (`HUM_CODIGO`, `HUM_HUMOR`) VALUES
+INSERT INTO `TB_HUMORES` (`HUM_CODIGO`, `HUM_HUMOR`) VALUES
 (1, 'Sonolento'),
 (2, 'Triste'),
 (3, 'Estressado'),
@@ -56,7 +70,7 @@ INSERT INTO `tb_humores` (`HUM_CODIGO`, `HUM_HUMOR`) VALUES
 -- Estrutura da tabela `tb_estado_saude`
 --
 
-CREATE TABLE `tb_est_saude` (
+CREATE TABLE `TB_EST_SAUDE` (
   `EST_CODIGO` int(11) NOT NULL auto_increment PRIMARY KEY,
   `EST_GAT_CODIGO` int(11) NOT NULL,
   `EST_HUM_CODIGO` int(11) NOT NULL,
@@ -65,7 +79,7 @@ CREATE TABLE `tb_est_saude` (
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `tb_recover` (
+CREATE TABLE `TB_RECOVER` (
 
   `REC_CODIGO` int(20) NOT NULL auto_increment PRIMARY KEY,
   `REC_EMAIL` varchar(200) NOT NULL,
@@ -74,12 +88,12 @@ CREATE TABLE `tb_recover` (
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `tb_gatos` 
-	ADD CONSTRAINT FK_USUGAT FOREIGN KEY (GAT_USU_CODIGO) REFERENCES TB_USUARIOS(USU_CODIGO),
-	ADD CONSTRAINT FK_SEXGAT FOREIGN KEY (GAT_SEX_CODIGO) REFERENCES TB_SEXOS(SEX_CODIGO);
+ALTER TABLE `TB_GATOS` 
+	ADD CONSTRAINT `FK_USUGAT` FOREIGN KEY (GAT_USU_CODIGO) REFERENCES `TB_USUARIOS`(USU_CODIGO),
+	ADD CONSTRAINT `FK_SEXGAT` FOREIGN KEY (GAT_SEX_CODIGO) REFERENCES `TB_SEXOS`(SEX_CODIGO);
 COMMIT;
 
-ALTER TABLE `tb_est_saude` 
-	ADD CONSTRAINT FK_GATEST FOREIGN KEY (EST_GAT_CODIGO) REFERENCES TB_GATOS(GAT_CODIGO),
-	ADD CONSTRAINT FK_HUMEST FOREIGN KEY (EST_HUM_CODIGO) REFERENCES TB_HUMORES(HUM_CODIGO);
+ALTER TABLE `TB_EST_SAUDE` 
+	ADD CONSTRAINT `FK_GATEST` FOREIGN KEY (EST_GAT_CODIGO) REFERENCES `TB_GATOS`(GAT_CODIGO),
+	ADD CONSTRAINT `FK_HUMEST` FOREIGN KEY (EST_HUM_CODIGO) REFERENCES `TB_HUMORES`(HUM_CODIGO);
 COMMIT;
